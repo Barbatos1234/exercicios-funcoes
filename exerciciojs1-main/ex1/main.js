4da640b8df3fc718c15913bc4fd3b2d367b49274
Barbatos1234 <137101698+Barbatos1234@users.noreply.github.com>
2024-05-31 02:25:51 -0300

    Add files via upload

---
+++ exerciciojs1-main/ex1/main.js
@@ -0,0 +1,9 @@
+function saudacao() {
+
+    var nome = document.getElementById("nome").value;
+    alert ("Bem-vindo, " + nome );
+
+}
+
+
+
---
+++ exerciciojs1-main/ex2/main.js
@@ -0,0 +1,7 @@
+function contarCaracteres(){
+    // alert ("teste");
+    var texto = document.getElementById("texto").value;
+    var numCaracteres = texto.length
+    alert (numCaracteres);
+
+};
